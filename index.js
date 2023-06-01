4d6f5e264e2bbccea4c15a1ac3bd1805924179ca
MaxiResz <https://github.com/MaxiResz/Backend-ecommerce-Bootcampudd.git>
2023-06-01 10:30:48 -0400

    Tercer avance

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Importando las rutas y el middleware de manejo de errores
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/users');
+const errorHandler = require('./middleware/errorHandling');
+
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Usando las rutas
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+// Usando el middleware de manejo de errores
+app.use(errorHandler);
+
+// Iniciando el servidor
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
